dd33248550bcfc655bfee3a1956502e651813805
Justin Acance <jacance@Justins-MacBook-Pro.local>
2021-03-27 00:54:12 -0700

    draft table made in postgres

--- SQL/sqldrop.sql
+++ SQL/sqldrop.sql
@@ -16,6 +16,9 @@ WHERE player = 'Georgi Funtarov' AND "FGA" = 16
 SELECT * FROM career
 WHERE player = 'Anthony Mathis'
 
+SELECT * FROM draft
+WHERE player like 'Derrick Coleman'
+
 
 
 --Create primary key for career
@@ -28,3 +31,17 @@ ALTER TABLE draft
 ADD CONSTRAINT official_key
 FOREIGN KEY(official_key) 
 REFERENCES career(new_key)
+
+
+--Create draft table then import draft.csv
+CREATE TABLE draft (
+	"year" VARCHAR,
+	team_abb VARCHAR,
+	player VARCHAR,
+	college VARCHAR,
+	round VARCHAR,
+	pick VARCHAR,
+	official_key VARCHAR,
+FOREIGN KEY (official_key)
+REFERENCES career (new_key)
+);
