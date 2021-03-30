156c6588d31cf6a5afb2c355888ca1efd0cbd6d9
leonardz17 <71532759+leonardz17@users.noreply.github.com>
2021-03-29 23:04:20 -0700

    added contact.html and relevant js and css, added route to app.py for contact page

--- Flask/app.py
+++ Flask/app.py
@@ -3,12 +3,12 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify, render_template
-from config import user, pw, port
+# from config import user, pw, port
 
 
 # Create engine
-engine = create_engine(f'postgresql://{user}:{pw}@localhost:{port}/slam_dunk_analytics_db')
-connection = engine.connect()
+# engine = create_engine(f'postgresql://{user}:{pw}@localhost:{port}/slam_dunk_analytics_db')
+# connection = engine.connect()
 
 app = Flask(__name__)
 
@@ -18,6 +18,10 @@ app = Flask(__name__)
 def home():
     print("Server received request for 'Home' page...")
     return render_template("index.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
