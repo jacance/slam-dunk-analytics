6f5a2119c68291bd6686a18a5a66b7396f1ee352
Justin Acance <jacance@justins-mbp.lan>
2021-03-31 00:42:30 -0700

    jsonify sql combined table limit 100

--- Flask/app.py
+++ Flask/app.py
@@ -3,12 +3,13 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify, render_template
-# from config import user, pw, port
+from config import user, pw, port
+import pandas as pd
 
 
 # Create engine
-# engine = create_engine(f'postgresql://{user}:{pw}@localhost:{port}/slam_dunk_analytics_db')
-# connection = engine.connect()
+engine = create_engine(f'postgresql://{user}:{pw}@localhost:{port}/slam_dunk_analytics_db')
+connection = engine.connect()
 
 app = Flask(__name__)
 
@@ -18,6 +19,18 @@ app = Flask(__name__)
 def home():
     print("Server received request for 'Home' page...")
     return render_template("index.html")
+
+@app.route("/visualizations.html")
+def visualizations():
+    sql = """
+    SELECT *
+    FROM combined
+    LIMIT 100
+    """
+
+    print(sql)
+    results = pd.read_sql(sql, connection)
+    return jsonify(results.to_dict("data"))
 
 @app.route("/contact")
 def contact():
